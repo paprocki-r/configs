f39af96024ed5870d1f8f1dad845958cf19b6d71
Rafal Paprocki <rafal.paprocki@gmail.com>
2018-02-18 23:10:38 +0900

    tmux configuration and linking

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ apt install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 ln -s  $PWD/vim/.vimrc ~/.vimrc
 ln -s  $PWD/zsh/.zshrc ~/.zshrc
+ln -s  $PWD/tmux/.tmux.conf ~/.tmux.conf
