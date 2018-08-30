1fe5606909a2a4e9acb489443c74923d8ef8dcd6
Rafal Paprocki <r.paprocki@AMDC3708.digital.local>
2018-08-30 17:30:37 +0200

    small fixes

--- install.sh
+++ install.sh
@@ -1,25 +1,7 @@
 #!/bin/sh
-apt install terminator
 apt install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-mv  $PWD/vim/.vimrc ~/.vimrc_bak
-mv  $PWD/zsh/.zshrc ~/.zshrc_bak
-mv  $PWD/tmux/.tmux.conf ~/.tmux.conf_bak
-
+sudo apt-get install fonts-powerline #fonts for omzsh agnoster theme
 ln -s  $PWD/vim/.vimrc ~/.vimrc
 ln -s  $PWD/zsh/.zshrc ~/.zshrc
 ln -s  $PWD/tmux/.tmux.conf ~/.tmux.conf
-
-#set up terminator as default terminal
-gsettings set org.gnome.desktop.default-applications.terminal exec /usr/bin/terminator
-gsettings set org.gnome.desktop.default-applications.terminal exec-arg "-x"
-
-#@TODO: set up shortcut to terminator
-
-
-#set ZSH as default shell
-chsh -s $(zsh)
-
-#TMUX PLUGIN MANAGER installation
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
