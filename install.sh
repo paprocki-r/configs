ed96c9df452a6016137924bf43b810042fd0e67a
Rafal Paprocki <r.paprocki@AMDN3429VM01.digital.local>
2018-09-19 10:56:48 +0200

    small fixes

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ apt intall terminator
 apt install zsh
 apt install curl
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 sudo apt-get install fonts-powerline #fonts for omzsh agnoster theme
 ln -s  $PWD/vim/.vimrc ~/.vimrc
 ln -s  $PWD/zsh/.zshrc ~/.zshrc
@@ -35,6 +36,7 @@ apt install python3-dev python3-pip
 #thefuck
 pip3 install thefuck
 
+
 #i3wm-gaps installation 
 sh i3gapsinstall/install.sh
 sudo apt-get install dmenu
@@ -44,4 +46,23 @@ ln -s $PWD/i3-gaps-conf/config ~/.config/i3/config
 #for transparency
 ln -s $PWD/i3gapsinstall/.compton.conf ~/.compton.conf
 
+### extra software
+mkdir software
+## Pycharm
+# download
+curl https://download-cf.jetbrains.com/python/pycharm-community-2018.2.3.tar.gz --output ~ /software/pycharm.tar.gz
+# unpack
+tar -xzf pycharm.tar.gz
+rm pycharm-community-2018.2.3.tar.gz
+# add alias - pycharm will make it with first run
+#sudo ln -s  $PWD/software/pycharm-community-2018.2.3/bin/pycharm.sh /usr/local/bin/pycharm
+
+## sublime
+#download
+curl https://download.sublimetext.com/sublime_text_3_build_3176_x64.tar.bz2 --output sublime.tar.bz2
+#unpack
+tar xvjf sublime.tar.bz2
+rm sublime.tar.bz2
+#make a link
+sudo ln -s  $PWD/software/sublime_text_3/sublime_text /usr/local/bin/subl
 
